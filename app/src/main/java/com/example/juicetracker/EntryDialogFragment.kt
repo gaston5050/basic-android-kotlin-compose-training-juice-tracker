102d4611052f6897893840d7293af4b73a588eaa
gaston5050 <gaston14101985@gmail.com>
2026-09-20 22:43:47 -0300

    trabajando en empty dialog fragment

--- app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -1,4 +1,21 @@
 package com.example.juicetracker
 
-class EntryDialogFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.example.juicetracker.databinding.FragmentEntryDialogBinding
+
+
+class EntryDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentEntryDialogBinding.inflate(inflater, container,false).root
+    }
+
 }
