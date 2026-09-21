6268c09d56f2a8c689a89294261b94f625265d36
Gaston Ayala <gastona@dp.net>
2026-09-21 16:59:14 -0300

    Segundo commit

--- app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.example.juicetracker.databinding.FragmentEntryDialogBinding
+import com.example.juicetracker.ui.AppViewModelProvider
+import com.example.juicetracker.ui.EntryViewModel
 
 
 class EntryDialogFragment : BottomSheetDialogFragment() {
+
+    private val entryViewModel: EntryViewModel by viewModels<EntryViewModel>{
+        AppViewModelProvider.Factory
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
